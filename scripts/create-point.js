30fdce0183352f10881d6a586c9f9310786575d2
Pedro Emanuel Lima <pa69731@gmail.com>
2020-06-03 17:25:46 -0300

    Resultado do terceiro dia do NLW, adicionado uma página com pesquisas e um modal oculto para pesquisa.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -21,11 +21,14 @@ function getCities(event){
     const indexOfSetectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSetectedState].text
 
+    citySelect.innerHTML = "<option value=''>Selecione a cidade</option>"
+    citySelect.disabled = true
+
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`)
         .then(res => res.json() )
         .then(cities => {
             for(city of cities){
-                citySelect.innerHTML += `<option value=${city.id}>${city.nome}</opition>`
+                citySelect.innerHTML += `<option value=${city.nome}>${city.nome}</opition>`
             }
             citySelect.disabled = false
         });
@@ -36,3 +39,36 @@ document
     .querySelector('select[name=uf]')
     .addEventListener('change', getCities);
 
+ // Itens de coleta
+
+ const itemsTosCollect = document.querySelectorAll('.items-grid li');
+
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle("selected");
+    
+    const itemId = itemLi.dataset.id;
+    const alreadySelected = selectedItems.findIndex(item => item == itemId);
+
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDiferent = item != itemId;
+            return itemIsDiferent;
+        })
+        selectedItems = filteredItems
+    }else{
+        selectedItems.push(itemId)
+    }   
+
+    collectedItems.value = selectedItems;
+}
+
+
+
+for (item of itemsTosCollect){
+    item.addEventListener('click', handleSelectedItem)
+}
